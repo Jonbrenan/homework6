7ae37faa51ad27f7cdfa99755ce0274a12145cd3
Jon Brenan <jonathanbrenan@gmail.com>
2020-03-19 14:05:51 -0400

    At this point, when one searches a city into the search bar, the page will display its current temperature, humidity, and wind speed. However, if you search multiple times it will add the same information over and over again. The next commit will rectify this

--- java.js
+++ java.js
@@ -2,7 +2,7 @@
 
 
 let body=document.body
-
+let form=document.createElement('form')
 let leftDiv=document.createElement('div')
 let rightDiv=document.createElement('div')
 let searchBar=document.createElement('input')
@@ -10,14 +10,16 @@ let searchButton=document.createElement('button')
 let whereBttnsGo=document.createElement('div')
 let cityName=document.createElement('h2')
 
-
+form.setAttribute('class', 'container')
 leftDiv.setAttribute('id', 'leftDiv')
 searchBar.setAttribute('id','searchBar')
 searchButton.setAttribute('id', 'searchButton')
 whereBttnsGo.setAttribute('id','whereBttnsGo')
 searchButton.textContent="search"
 rightDiv.setAttribute('id', 'rightDiv')
+rightDiv.setAttribute('class', 'container')
 cityName.setAttribute('id', 'cityName')
+
 
 
 
@@ -61,8 +63,32 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + title + '&appid=bf
 })
 .then(function (data) {
     console.log(data)
+    console.log(data.list[0].wind.speed)
    let cityBigName=document.getElementById('cityName')
-   cityBigName.textContent=title
+   let weatherIcon=document.createElement('img')
+   let tempC=data.list[0].main.temp - 273.15
+   let tempF = tempC * (9 / 5) + 32
+   let currentTemp=document.createElement('h3')
+   let currentHumid=document.createElement('h3')
+   let currentWind=document.createElement('h3')
+   
+   
+   let currentDate=data.list[0].dt_txt
+   let weatherCode=data.list[0].weather[0].icon
+   let weatherCondition = 'http://openweathermap.org/img/wn/' + weatherCode + '@2x.png'
+   weatherIcon.setAttribute('src', weatherCondition)
+   currentTemp.textContent= 'Temperature: ' + tempF.toFixed(1) + '°F'
+   currentHumid.textContent=data.list[0].main.humidity + '%'
+   currentWind.textContent=data.list[0].wind.speed+ ' MPH'
+
+
+
+
+   cityBigName.textContent=title + " " + currentDate
+   rightDiv.appendChild(currentTemp)
+   rightDiv.appendChild(currentHumid)
+   rightDiv.appendChild(currentWind)
+   cityName.appendChild(weatherIcon)
    
 })
 
@@ -78,10 +104,13 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + title + '&appid=bf
 
 
 
+body.appendChild(form)
+form.appendChild(cityName)
 
-body.appendChild(leftDiv)
 body.appendChild(rightDiv)
-leftDiv.appendChild(searchBar)
+
+form.appendChild(searchBar)
+form.appendChild(leftDiv)
 leftDiv.appendChild(searchButton)
 leftDiv.appendChild(whereBttnsGo)
 rightDiv.appendChild(cityName)
